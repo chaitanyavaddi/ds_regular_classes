cf2eb97d92dcdce77860d5c3c23b441465ed095d
Chaitanya Vaddisriram <53746340+chaitanyavaddi@users.noreply.github.com>
2025-11-21 13:54:32 +0530

    added datasets

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import pandas as pd
 
-a = [1, 2, 3, 7, 5]
 
-for i in a:
-    print(i)
+df = pd.read_csv("/Users/chaitanyavaddi/Documents/01_met/datascience/ds_regular_classes/datasets/Sample_ Superstore.csv")
+
+#1. Get 5 by 5 rows and columns within dataset
+# print(df.head())
+
+#2. Get datatypes of columns
+# print(df.info())
+
+#3. Summary Statistics - #mean, standard deviation, min/max values, unexpected values
+# print(df.describe())
+
+#4. Missing values - Empty entries
+# print(df.isnull().sum())
+
+#5. Duplicates - Exact same row appearing more than once
+# Business impact - duplicate rows = wrong revenue calculation
+# print(df.duplicated().sum())
+
+# 6. Ouliers 
+# very large or very small values
+
+#Assigment
+# Choose a dataset from kaggle
+# Do priliminary analysis
+# Fund data quality issues
